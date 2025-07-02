713ea6420351fc4d01687d86ff2cccf5e03a3407
Jhonatan-Keb <jhonykeb123@gmail.com>
2025-07-02 11:30:08 -0600

    MapStruct implemented

--- src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
+++ src/main/java/com/tecdesoftware/market/persistence/ProductoRepository.java
@@ -2,9 +2,13 @@ package com.tecdesoftware.market.persistence;
 
 import com.tecdesoftware.market.persistence.crud.ProductoCrudRepository;
 import com.tecdesoftware.market.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+//Le dice a Spring que este repositorio se conecta con la DB
+@Repository
 public class ProductoRepository {
 
     private ProductoCrudRepository productoCrudRepository;
@@ -14,5 +18,28 @@ public class ProductoRepository {
         productoCrudRepository.findAll();
         //Convirtiendo un Iterable<t> a una lista de productos List<Producto>
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos (int cantidad) {
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    //Obtener un producto dado el id
+    public Optional<Producto> findByIdP(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    //Guardar Prodicto
+    public Producto save(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    //Borrar producto
+    public void delete(Producto producto) {
+        productoCrudRepository.delete(producto);
     }
 }
